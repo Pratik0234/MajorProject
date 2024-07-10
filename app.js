2c70d2eae1a7cc0b7294ec19823388b12443e34c
Pratik0234 <pwagh020304@gmail.com>
2024-07-10 22:38:07 +0530

    commit

--- app.js
+++ app.js
@@ -22,18 +22,11 @@ const userRouter = require("./routes/user.js");
 
 
 
-const dbUrl=process.env.ATLASDB_URL;
-main()
-.then(()=>{
-    console.log("connection to DB");
-})
-.catch(err=>{
-    console.log(err);
-});
-
-async function main(){
-    // await mongoose.connect(MONGO_URL);
-    await mongoose.connect(dbUrl);
+const dbUrl = process.env.ATLASDB_URL;
+main().then((res) => { console.log("connection to DB") })
+  .catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -63,7 +56,7 @@ const sessionOptions = {
   cookie: {
       expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
+      httpsOnly: true,
   },
 };
 
