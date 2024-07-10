7c6bc8843725ea74e0226e018df96a8c0f8b62e5
Pratik0234 <pwagh020304@gmail.com>
2024-07-10 15:57:18 +0530

    Initial commit

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,7 @@ const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/reviews.js");
 const Listing = require("../models/listing.js");
 // const { merge } = require("./listing.js");
-const {validateReview, isLoggedIn , isReviewAuthor} = require("../Middleware.js");
+const {validateReview, isLoggedIn , isReviewAuthor} = require("../middleware.js");
 
 const reviewController = require("../controllers/reviews.js");
 
