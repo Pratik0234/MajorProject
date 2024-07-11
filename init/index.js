1d6fd6ee7a99a62fe6060f3e3c4372f09c1ec754
Pratik0234 <pwagh020304@gmail.com>
2024-07-11 21:33:42 +0530

    commit

--- init/index.js
+++ init/index.js
@@ -1,9 +1,8 @@
-const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 
-const MONGO_URL= "mongodb+srv://pwagh020304:9ZDAYgFmysXJoLFf@cluster0.8gxf7cv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
 main()
 .then(()=>{
     console.log("Connected to DB");
@@ -18,6 +17,7 @@ async function main(){
 
 
 const initDB = async() => { 
+    await Listing.deleteMany({});
     initData.data = initData.data.map((obj) => ({...obj , owner : "668e64ad842b2e82558f1685"}));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
